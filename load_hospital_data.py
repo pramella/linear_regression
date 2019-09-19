076d583f88f9030e73d1b6be9f46804b4a20b829
paulohenrique-ramos <PauloHenrique.Ramos@chicagobooth.edu>
2019-09-19 14:52:01 -0500

    Added first version of load_hospital_data

---
+++ load_hospital_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+def load_hospital_data(path_to_data):
+    """
+    Loads the hospital charges data set found at data.gov.
+    INPUT
+        path_to_data (str) indicates the filepath to the hospital charge data (csv)
+    RETURNS
+        clean_df (pandas.DataFrame) containing the cleaned and formatted dataset for regression
+    """
+    clean_df = pd.read_csv(path_to_data)
+    return(clean_df)
