569e4772bd69955f068206bc1b70799813a3d2f3
paulohenrique-ramos <PauloHenrique.Ramos@chicagobooth.edu>
2019-09-19 15:21:10 -0500

    Added rest of the function from Derek's solution; let' see how git merges them

--- linear_regression.py
+++ linear_regression.py
@@ -17,7 +17,7 @@ def simulate_data():
     pass
 
 
-def compare_models():
+def compare_models(X, y, beta=None):
     """
     Compares output from different implementations of OLS.
     INPUT
@@ -26,10 +26,26 @@ def compare_models():
     RETURNS
         results (pandas.DataFrame) of estimated beta coefficients
     """
-    pass
+
+    # Using statsmodels OLS
+    output_sm = sm.OLS(y, X).fit()
+    beta_sm = output_sm.params
+
+    # Using sklearn's Linear Regression
+    output_skl = LinearRegression(fit_intercept=False).fit(X, y)
+    beta_skl = output_skl.coef_
+
+    results = pd.DataFrame()
+    results['statsmodels'] = beta_sm
+    results['sklearn'] = beta_skl
+
+    if beta is not None:
+        results['truth'] = beta
+
+    return results
 
 
-def load_hospital_data():
+def load_hospital_data(path_to_data):
     """
     Loads the hospital charges data set found at data.gov.
     INPUT
@@ -37,10 +53,42 @@ def load_hospital_data():
     RETURNS
         clean_df (pandas.DataFrame) containing the cleaned and formatted dataset for regression
     """
-    pass
+
+    # load csv with pandas
+    df = pd.read_csv(path_to_data)
+
+    # rename/reformat the columns
+    rename_dict = dict()
+    for c in df.columns:
+        rename_dict[c] = c.lower().strip()
+    df = df.rename(columns=rename_dict)
+
+    # drop unwanted variables
+    vars_to_drop = ['hospital referral region description',
+                    'provider street address',
+                    'provider name',
+                    'provider city',
+                    'provider zip code']
+    df = df.drop(vars_to_drop, axis=1)
+
+    # match provider id with provider state and condense the dataset
+    clean_df = pd.pivot_table(df, columns=['provider id'], aggfunc=sum).T
+
+    states_dict = dict()
+    for pid in clean_df.index:
+        states_dict[pid] = df[df['provider id']==pid]['provider state'].unique()[0]
+
+    clean_df['provider state'] = clean_df.index
+    clean_df['provider state'] = clean_df['provider state'].map(states_dict)
+    
+    # ensure integers where appropriate
+    clean_df['total discharges'] = clean_df['total discharges'].astype(np.int64)
 
 
-def prepare_data():
+    return clean_df
+
+
+def prepare_data(df):
     """
     Prepares hospital data for regression (basically turns df into X and y).
     INPUT
@@ -48,10 +96,21 @@ def prepare_data():
     RETURNS
         data (dict) containing X design matrix and y response variable
     """
-    pass
+    data = dict()
+
+    X1 = np.log(df['total discharges'].values)
+    X2 = pd.get_dummies(df['provider state'])
+    X = np.column_stack((np.ones(len(X1)), X1, X2))
+
+    Y = np.log(df['average covered charges'].values)
+
+    data['X'] = X
+    data['y'] = Y
+
+    return data
 
 
-def run_hospital_regression():
+def run_hospital_regression(path_to_data):
     """
     Loads hospital charge data and runs OLS on it.
     INPUT
@@ -59,7 +118,11 @@ def run_hospital_regression():
     RETURNS
         results (str) the statsmodels regression output
     """
-    pass
+    df = load_hospital_data(path_to_data)
+    data = prepare_data(df)
+    results = sm.OLS(data['y'], data['X']).fit().summary().as_text()
+
+    return results
  
 
 ### END ###
